ac56d61eb37d315d7e789162973517288117ffef
JoseGarciaEspinosa <github.jgge@gmail.com>
2017-01-27 20:35:21 -0600

    temperaturas

--- resources/views/vendor/adminlte/layouts/Temperaturas/Temperaturas.blade.php
+++ resources/views/vendor/adminlte/layouts/Temperaturas/Temperaturas.blade.php
@@ -1,4 +1,9 @@
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>		
+
+@for ($i = 0; $i < 10; $i++)
+	
 <script type="text/javascript">
 $(function () {
     $(document).ready(function () {
@@ -8,7 +13,7 @@ $(function () {
             }
         });
 
-        Highcharts.chart('container', {
+        Highcharts.chart('{{$i}}', {
             chart: {
                 type: 'spline',
                 animation: Highcharts.svg, // don't animate in old IE
@@ -76,93 +81,11 @@ $(function () {
         });
     });
 });
-		</script>
-	
-	
-	
-	<script type="text/javascript">
-$(function () {
-    $(document).ready(function () {
-        Highcharts.setOptions({
-            global: {
-                useUTC: false
-            }
-        });
+</script>
 
-        Highcharts.chart('container2', {
-            chart: {
-                type: 'spline',
-                animation: Highcharts.svg, // don't animate in old IE
-                marginRight: 10,
-                events: {
-                    load: function () {
 
-                        // set up the updating of the chart each second
-                        var series = this.series[0];
-                        setInterval(function () {
-                            var x = (new Date()).getTime(), // current time
-                                y = Math.random();
-                            series.addPoint([x, y], true, true);
-                        }, 1000);
-                    }
-                }
-            },
-            title: {
-                text: 'Temperatura Tunel'
-            },
-            xAxis: {
-                type: 'datetime',
-                tickPixelInterval: 150
-            },
-            yAxis: {
-                title: {
-                    text: 'Grados'
-                },
-                plotLines: [{
-                    value: 0,
-                    width: 1,
-                    color: '#808080'
-                }]
-            },
-            tooltip: {
-                formatter: function () {
-                    return '<b>' + this.series.name + '</b><br/>' +
-                        Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
-                        Highcharts.numberFormat(this.y, 2);
-                }
-            },
-            legend: {
-                enabled: false
-            },
-            exporting: {
-                enabled: false
-            },
-            series: [{
-                name: 'Random data',
-                data: (function () {
-                    // generate an array of random data
-                    var data = [],
-                        time = (new Date()).getTime(),
-                        i;
-
-                    for (i = -19; i <= 0; i += 1) {
-                        data.push({
-                            x: time + i * 1000,
-                            y: Math.random()
-                        });
-                    }
-                    return data;
-                }())
-            }]
-        });
-    });
-});
-		</script>
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
  <div class="col-md-6">
-<div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+<div id="{{$i}}" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
 </div>
- <div class="col-md-6">
-<div id="container2" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
-</div>
+
+@endfor
--- resources/views/vendor/adminlte/layouts/app.blade.php
+++ resources/views/vendor/adminlte/layouts/app.blade.php
@@ -19,7 +19,7 @@
                      <div class="col-md-12">
                         <div class="box">
                            <div class="box-header with-border">
-                              <h3 class="box-title">Panel de Control</h3>
+                              <h3 class="box-title">Panel de Control de Temperaturas</h3>
                               <div class="box-tools pull-right">
                                  <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
                                  </button>
@@ -31,6 +31,29 @@
                         <div class="row">
                           
                             @include('adminlte::layouts.Temperaturas.Temperaturas') 
+                         
+                        </div>
+                     </div>
+                        </div>
+                     </div>
+					 
+					 
+					 
+					 
+					 <div class="col-md-12">
+                        <div class="box">
+                           <div class="box-header with-border">
+                              <h3 class="box-title">Panel de Control Almacen</h3>
+                              <div class="box-tools pull-right">
+                                 <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                                 </button>
+                                 <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                              </div>
+                           </div>
+                           <!-- /.box-header -->
+                           <div class="box-body">
+                        <div class="row">
+                          
                          
                         </div>
                      </div>
